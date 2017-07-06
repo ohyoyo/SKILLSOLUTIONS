2f36fb3060e21b9b8d0e3735767daa6a97ab373a
Yoann Di Maria <dm.yoann@gmail.com>
2017-07-06 23:19:25 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// REQUIRE AND EXECUTE MODULE EXPRESS
+const express = require('express');
+const app = express();
+
+// REQUIRE AND EXECUTE MODULE EJS
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+app.use('/assets/css/', express.static(__dirname+'/www/links/css'));
+app.use('/assets/js/', express.static(__dirname+'/www/links/js'));
+app.use('/assets/img/', express.static(__dirname+'/www/links/img'));
+app.use('/assets/fonts/', express.static(__dirname+'/www/links/fonts'));
+
+
+const params = {
+    sitename    : 'Skillsolutions Software'
+}
+
+// VIEWS
+app.get('/', function(req, res) {
+    res.render(__dirname+'/www/views/landingpage', {params:params});
+});
+
+// OPEN PORT
+app.listen(1337);
