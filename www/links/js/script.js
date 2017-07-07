86d5516de1bf83a5ac11493ef32e637a895f00a2
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-07-07 07:16:08 +0200

    dg

--- www/links/js/script.js
+++ www/links/js/script.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     $('#burger').click(function () {
         this.classList.toggle('active');
         if (this.className == 'active') {
+            $('body').css('overflow', 'hidden');
             $('header nav ul').fadeIn('low');
         } else {
             $('header nav ul').fadeOut('low');
+            $('body').css('overflow', 'visible');
         }
     });
 });
